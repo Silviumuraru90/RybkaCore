c5f409499b09ea9cf2103ec85d9f59fb70231251
Silviu Muraru <silviumuraru90@gmail.com>
2023-01-16 19:40:21 +0000

    Do NOT let Binance's exit code override Rybka's custom set exit code 7

--- rybka.py
+++ rybka.py
@@ -610,11 +610,14 @@ def on_close(ws, close_status_code, close_msg):
 def on_error(ws, message):
     if str(message).strip():
         print(f"{bcolors.CRED}{bcolors.BOLD}❌ FATAL {log.logging_time()}      > Software encountered an error and got shutdown! Additional error message provided:\n{message}{bcolors.ENDC}")
-        log.all_errors_file_update(f"❌ FATAL (1) {log.logging_time()}      > Software encountered an error and got shutdown! Additional error message provided:\n{message}")
+        log.all_errors_file_update(f"❌ FATAL {log.logging_time()}      > Software encountered an error and got shutdown! Additional error message provided:\n{message}")
+        if str(message).strip() == "7":
+            exit(7)
     else:
         print(f"{bcolors.CRED}{bcolors.BOLD}❌ FATAL {log.logging_time()}      > Software encountered an error and got shutdown! No additional error message provided{bcolors.ENDC}")
-        log.all_errors_file_update(f"❌ FATAL (1) {log.logging_time()}      > Software encountered an error and got shutdown! No additional error message provided")
-
+        log.all_errors_file_update(f"❌ FATAL {log.logging_time()}      > Software encountered an error and got shutdown! No additional error message provided")
+        if str(message).strip() == "7":
+            exit(7)
 
 def on_message(ws, message):
     global uptime
