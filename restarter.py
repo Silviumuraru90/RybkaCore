0dbfbcfba1e5aa3122158bc8fc0ac85e46a3b476
Silviu Muraru <silviumuraru90@gmail.com>
2023-01-21 10:45:41 +0200

    Switch restarter from a 10-try loop to an infinite one when internet is down

--- restarter.py
+++ restarter.py
@@ -69,30 +69,23 @@ def main(version, mode, info):
                 log.FATAL(f" 🔴 Rybka bot errored out with code [{status}].\n\nExit code '7' is a custom-set exit code within bot's mechanics. Manual action is required, both Rybka and restarter module are stopped.")
             else:
                 log.WARN(f" 🟠 Rybka bot errored out with code [{status}], which is not a custom-hard-coded one. Proceeding to check internet availability...")
-                internet_down = False
-                for i in range(1, 11):
+                while True:
                     log.INFO(" ")
-                    log.INFO(f" 🟣 Testing internet. Attempt [{i}/10]")
+                    log.INFO(f" 🟣 Checking internet connection")
                     try:
                         r = requests.get("https://www.google.com")
                         log.INFO(f" 🟢 🔌 Could ping Google servers, internet should be on - response [{r.status_code}] returned")
                         time.sleep(5)
+                        log.INFO(" 🟢 Restart action sent.")
                         break
                     except Exception as e:
-                        internet_down = True
                         log.WARN(" 🟠 🔌 Internet seems to not be available")
                         log.INFO(" 🟣 Waiting 15 seconds and trying again")
                         time.sleep(15)
-                if not internet_down:
-                    log.VERBOSE(" 🟣 Rybka did not catch the internet connection as being down. Hence it might've been a different cause of failure")
-                    log.INFO(" 🟣 Applying a 1-minute break and will restart Rybka bot afterwards!")
-                    time.sleep(60)
-                else:
-                    log.INFO(" 🟢 Restart action sent.")
-            runs+=1
-            if running_in_ci and runs == 3:
-                log.FATAL(" 🔴 This is a CI run. Breaking the process now after the 3 attempts.")
-
+                    runs+=1
+                    if running_in_ci and runs == 3:
+                        log.FATAL(" 🔴 This is a CI run. Breaking the process now after the 3 attempts.")
+                
 
 if __name__ == '__main__':
     main()
